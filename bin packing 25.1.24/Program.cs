7701e645b9bee1cbed5716c255dca701837f5887
Liam Bolan <AQ231688@aquinas.ac.uk>
2024-01-25 13:24:27 +0000

    Add project files.

---
+++ bin packing 25.1.24/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bin_packing_25._1._24
+{
+    internal class Program
+    {
+        struct bin
+        {
+            public int capacity;
+            public string packages;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Input Bin size:  ");
+            int binsz = int.Parse(Console.ReadLine());
+            Console.Write("Input packet sizes(x,x,x,x,x,x..): ");
+            string packsz = Console.ReadLine();
+            string[] packsizes;
+            packsizes = packsz.Split(',');
+
+            for(int i = 0; i < packsizes.Length; i++)
+            {
+                
+            }
+
+            List<bin> bins = new List<bin>();
+
+            bin b;
+            b.capacity = binsz;
+            b.packages = "";
+            bins.Add(b);
+            bin thisbin = bins[0];
+            thisbin.packages += packsizes[0] + ",";
+            thisbin.capacity -= int.Parse(packsizes[0]);
+
+            bins[0] = thisbin;
+
+            
+
+
+        }
+    }
+}
